cb3c9fca3806c90730bd6c94780d6bf1d1e9bb08
Brandon Sneed <brandon@smallsociety.com>
2009-08-13 14:14:09 -0700

    Removed some comment references.

---
+++ TouchUI/iPhone/Views/CBetterLabel.h
@@ -0,0 +1,26 @@
+//
+//  CBetterLabel.h
+//
+//  Created by brandon on 7/13/09.
+//  Copyright 2009 Small Society. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+typedef enum
+{
+	UITextVerticalAlignmentCenter,
+	UITextVerticalAlignmentTop,
+	UITextVerticalAlignmentBottom
+} UITextVerticalAlignment;
+
+@interface CBetterLabel : UILabel
+{
+	UITextVerticalAlignment verticalAlignment;
+	BOOL variableHeightText;
+}
+
+@property (assign, nonatomic) UITextVerticalAlignment verticalAlignment;
+@property (assign, nonatomic) BOOL variableHeightText;
+
+@end
