0b900b77a1d3503863708ed860b3ed4890f2f36b
Jonathan Wight <jwight@mac.com>
2009-11-15 11:34:32 -0500

    Automated merge with https://touchcode.googlecode.com/hg/

---
+++ Support/Common/CHumanDateFormatter.h
@@ -0,0 +1,23 @@
+//
+//  CHumanDateFormatter.h
+//  ProjectV
+//
+//  Created by Jonathan Wight on 9/12/08.
+//  Copyright 2008 toxicsoftware.com. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CHumanDateFormatter : NSDateFormatter {
+	BOOL singleLine;
+	BOOL relative;
+}
+
+@property (readwrite, nonatomic, assign) BOOL singleLine;
+@property (readwrite, nonatomic, assign) BOOL relative;
+
++ (id)humanDateFormatter:(BOOL)inSingleLine;
+
++ (NSString *)formatDate:(NSDate *)inDate singleLine:(BOOL)inSingleLine;
+
+@end
